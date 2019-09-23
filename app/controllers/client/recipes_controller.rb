fb883e5c6347585cc0262f0949a451dd33010885
JackMarx <seriousfools@gmail.com>
2019-09-23 16:22:48 -0500

    frontend show

--- app/controllers/client/recipes_controller.rb
+++ app/controllers/client/recipes_controller.rb
@@ -4,4 +4,10 @@ class Client::RecipesController < ApplicationController
     @recipes = response.parse
     render 'index.html.erb'
   end
+
+  def show
+    response = HTTP.get("http://localhost:3000/api/recipes/#{params[:id]}")
+    @recipe = response.parse
+    render 'show.html.erb'
+  end
 end
