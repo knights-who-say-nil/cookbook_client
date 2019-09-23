8ccc8ecf95e3fe08cbe6c202537bcaa2fa57d42f
JackMarx <seriousfools@gmail.com>
2019-09-23 16:54:11 -0500

    new form/route

--- app/controllers/client/recipes_controller.rb
+++ app/controllers/client/recipes_controller.rb
@@ -5,6 +5,10 @@ class Client::RecipesController < ApplicationController
     render 'index.html.erb'
   end
 
+  def new
+    render 'new.html.erb'
+  end
+
   def show
     response = HTTP.get("http://localhost:3000/api/recipes/#{params[:id]}")
     @recipe = response.parse
