02b7c0efb5eefde69280cafe229f530316347642
JackMarx <seriousfools@gmail.com>
2019-09-24 11:09:18 -0500

    edit/update

--- app/controllers/client/recipes_controller.rb
+++ app/controllers/client/recipes_controller.rb
@@ -30,4 +30,26 @@ class Client::RecipesController < ApplicationController
     @recipe = response.parse
     render 'show.html.erb'
   end
+
+  def edit
+    response = HTTP.get("http://localhost:3000/api/recipes/#{params[:id]}")
+    @recipe = response.parse
+    render 'edit.html.erb'
+  end
+
+  def update
+    response = HTTP.patch(
+                          "http://localhost:3000/api/recipes/#{params[:id]}",
+                          form: {
+                                  title: params[:title],
+                                  chef: params[:chef],
+                                  prep_time: params[:prep_time],
+                                  ingredients: params[:ingredients],
+                                  directions: params[:directions],
+                                  image_url: params[:image_url]
+                                }
+                          )
+    @recipe = response.parse
+    redirect_to "/client/recipes/#{@recipe['id']}"
+  end
 end
